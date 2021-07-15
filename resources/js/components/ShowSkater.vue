23a4e1d6e3adaa26a389a5f1e0e4741765d1fb20
patrick <patrickodey419@gmail.com>
2021-07-15 19:22:52 +0100

    filled in the component

---
+++ resources/js/components/ShowSkater.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <div class="card" style="width: 18rem;" v-bind:key="skater.id" v-for="skater in skate">
+      <img src="https://images.pexels.com/photos/3274903/pexels-photo-3274903.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
+      <div class="card-body" >
+        <p class="card-title">Name: {{skater.name}}</p>
+        <p class="card-text">Age: {{skater.age}}</p>
+        <p class="card-text">State: {{skater.state}}</p>
+        <p class="card-text">Stance: {{skater.stance}}</p>
+        <a href="#" class="btn btn-primary">See More...</a>
+      </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['skate'],
+
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
